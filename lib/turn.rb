d7a0af0d6db436009e29f61b6e42c28631fc61bc
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-09-29 17:46:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,3 @@
-
-
 def display_board(board)
    puts " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
    puts "-----------"
@@ -28,4 +26,8 @@ end
 def move(board, index, character = "X")
    board[index] = character
    return board
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
 end
